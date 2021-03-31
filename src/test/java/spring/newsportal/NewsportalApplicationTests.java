0c78e6f69c748cdc5dd7bd0fefe852c65fc8c148
Dos06 <dostermorgan815@gmail.com>
2021-04-01 00:10:41 +0600

    Initial commit

---
+++ src/test/java/spring/newsportal/NewsportalApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.newsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
